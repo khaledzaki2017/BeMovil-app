57a644484813123637a563df8446b0f401789a0c
khaledzaki2017 <khaledzaky07@gmail.com>
2020-10-08 01:50:08 +0200

    update seetings.py and readme.md files

--- BeMovileApp/settings.py
+++ BeMovileApp/settings.py
@@ -185,9 +185,9 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 # EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_USER = 'khaledzaky07@gmail.com'
+EMAIL_HOST_USER = '################'
 # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_PASSWORD = 'khaled39052159'
+EMAIL_HOST_PASSWORD = '***********'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # BeMovil-app
 BeMovil-app-api
 # requires
-python_version = "3.6"
+python version == "3.7.2"
+pip version ==18.1
+
+YOU NEED TO SET  ==> EMAIL_HOST_USER       in BeMovileApp/settings.py
+YOU NEED TO SET  ==> EMAIL_HOST_PASSWORD   in BeMovileApp/settings.py
+IMPORTANT ==> YOU MUST TURN ON "Less secure app access" FROM THIS LINK IF YOU WILL USE GMAIL "https://myaccount.google.com/lesssecureapps"
