855f42e6e6d6c22dc9492bdba7296d69daeafa04
khaledzaki2017 <khaledzaky07@gmail.com>
2020-10-24 16:51:54 +0200

    fix

--- multi_step_form/views.py
+++ multi_step_form/views.py
@@ -47,7 +47,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import viewsets, status
 from rest_framework.views import APIView
-from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser, FileUploadParser
 import logging
 logger = logging.getLogger('django')
 # **************************************************************************************
